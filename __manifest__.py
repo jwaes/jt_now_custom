7964ac4f8eb780ed05f7b37660c149d1e1bb5979
jwaes <jaco.waes@gmail.com>
2022-03-07 10:36:50 +0000

    added search

--- __manifest__.py
+++ __manifest__.py
@@ -13,10 +13,10 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '15.0.1.0.3',
+    'version': '15.0.1.0.4',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','crm','project'],
+    'depends': ['base','crm','project','purchase'],
 
     # always loaded
     'data': [
