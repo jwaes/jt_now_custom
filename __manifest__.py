51488e564c76a0411005145ee20b9541f55d3f44
jwaes <jaco.waes@gmail.com>
2022-04-28 10:09:53 +0000

    bump versionnumber

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '15.0.1.0.10',
+    'version': '15.0.1.0.11',
 
     # any module necessary for this one to work correctly
     'depends': ['base','crm','project','purchase','sale_purchase','jt_mrp_otf','jt_lead_task'],
