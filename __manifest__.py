c3efdf092b798aff1a1d3a948e8aa364b5c08fe9
jwaes <jaco.waes@gmail.com>
2022-07-06 09:22:14 +0000

    updates

--- __manifest__.py
+++ __manifest__.py
@@ -14,17 +14,30 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '15.0.1.0.12',
+    'version': '1.15',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale_management','crm','project','purchase','sale_purchase','jt_mrp_otf','jt_lead_task'],
+    'depends': [
+        'base',
+        'website_sale_stock',
+        'sale_management',
+        'crm',
+        'project',
+        'purchase',
+        'sale_purchase',
+        'jt_mrp_otf',
+        'jt_lead_task',
+        ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         "report/purchase_quotation_templates.xml",
         "report/purchase_order_templates.xml",
+        "report/sale_report_templates.xml",
         "views/crm_lead_views.xml",
+        "views/ir_qweb_widget_templates.xml",
+        "views/portal_templates.xml",
         "views/res_partner_views.xml",
         "views/stock_picking_views.xml",
         "views/product_view.xml",
@@ -37,6 +50,9 @@
     'assets': {
         'web._assets_primary_variables': [
             'jt_now_custom/static/src/legacy/scss/primary_variables.scss',
+        ],
+        'web.assets_qweb': [
+            'jt_now_custom/static/src/xml/*.xml',
         ],
     },
 }
