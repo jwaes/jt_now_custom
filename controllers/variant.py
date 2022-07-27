e32bd29b0b3e3f9d664bc8352b90a31b2bc76ef7
jwaes <jaco.waes@gmail.com>
2022-07-27 09:10:49 +0000

    permissions

--- controllers/variant.py
+++ controllers/variant.py
@@ -8,7 +8,7 @@ class WebsiteSaleStockPropertiesVariantController(WebsiteSaleVariantController):
         kw['context'] = kw.get('context', {})
         combination = super().get_combination_info_website(product_template_id, product_id, combination, add_qty, **kw)
         
-        product = request.env['product.product'].browse(combination['product_id'])
+        product = request.env['product.product'].sudo().browse(combination['product_id'])
 
         stockinfo_view = request.env['ir.ui.view']._render_template('jt_now_custom.website_sale_product_stockinfo', values={
             'product_variant': product,
