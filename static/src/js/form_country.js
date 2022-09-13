10a979f7fec6d48acf63f4c5f7234b745dd59426
jwaes <jaco.waes@gmail.com>
2022-09-13 09:43:49 +0000

    minimize

--- static/src/js/form_country.js
+++ static/src/js/form_country.js
@@ -1,27 +1,14 @@
 /** @odoo-module */
 
-import { get_cookie } from 'web.utils.cookies';
 import { session } from '@web/session';
 
 $(document ).ready(function() {
     const slct = $('select[name=country_id].s_website_form_input');
     if(slct.length){
-        console.log("select is there");
-        console.log(slct);
-
         if (session.geoip_country_name){
-            console.log("country name is " + session.geoip_country_name);
-
             $('select[name=country_id].s_website_form_input option').filter(function(){
-                console.log($(this).text())
                 return $(this).text() == session.geoip_country_name;
             }).prop('selected',true);
-
-        } else {
-            console.log("no geoip_country_name found");
         }
-
-    } else {
-        console.log("select not found");
-    }
+    } 
 });
