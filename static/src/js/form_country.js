7db24b823e215eaafabffde32ff1babf0d032503
jwaes <jaco.waes@gmail.com>
2022-09-13 09:09:56 +0000

    geoip form default

--- static/src/js/form_country.js
+++ static/src/js/form_country.js
@@ -4,16 +4,23 @@ import { get_cookie } from 'web.utils.cookies';
 import { session } from '@web/session';
 
 $(document ).ready(function() {
-    const slct = $('select.s_website_form_input');
+    const slct = $('select[name=country_id].s_website_form_input');
     if(slct.length){
         console.log("select is there");
-        const htmlEl = document.documentElemen;
-        const country = session.geoip_country_code;
-        console.log("session is " + session);
-        console.log(session);
-        if (country) {
-            console.log("country is " + country);
+        console.log(slct);
+
+        if (session.geoip){
+            console.log("country name is " + session.geoip_country_name);
+
+            $('select[name=country_id].s_website_form_input option').filter(function(){
+                console.log($(this).text())
+                return $(this).text() == session.geoip_country_name;
+            }).prop('selected',true);
+
+        } else {
+            console.log("no geoip found");
         }
+
     } else {
         console.log("select not found");
     }
