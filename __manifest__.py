6bf216533ee151ec8df251abda4fc9b457800457
jwaes <jaco.waes@gmail.com>
2022-10-04 11:18:13 +0000

    show prices

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '1.43',
+    'version': '1.44',
 
     # any module necessary for this one to work correctly
     'depends': [
