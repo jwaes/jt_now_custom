de7bbf0a7819763141dda5315849e81a2a4e25da
jwaes <jaco.waes@gmail.com>
2022-12-06 09:30:40 +0000

    week logging

--- models/purchase.py
+++ models/purchase.py
@@ -1,5 +1,7 @@
 from odoo import api, fields, models
 
+_logger = logging.getLogger(__name__)
+
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
@@ -8,4 +10,9 @@ class PurchaseOrderLine(models.Model):
     @api.depends('date_planned')
     def _compute_week_planned(self):
         for rec in self:
-            rec.week_planned = rec.date_planned.isocalendar()[1]
+            if rec.date_planned:
+                _logger.info("Purchase order line date planned: ", rec.date_planned)
+                rec.week_planned = rec.date_planned.isocalendar()[1]
+                _logger.info("Week planned: ", rec.week_planned)
+            else:
+                _logger.warning("Purchase order line has no date planned")
