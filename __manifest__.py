f7c75a85c8848fa9618a117c5a6fccf871650677
jwaes <jaco.waes@gmail.com>
2023-03-20 16:18:56 +0000

    cust

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '1.77',
+    'version': '1.78',
 
     # any module necessary for this one to work correctly
     'depends': [
