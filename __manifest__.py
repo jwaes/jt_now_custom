60682712a8d6b3d7a21133e56fcdd5e47616df4e
jwaes <jaco.waes@gmail.com>
2023-03-28 09:12:08 +0000

    square hack

--- __manifest__.py
+++ __manifest__.py
@@ -14,12 +14,13 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '1.84',
+    'version': '1.85',
 
     # any module necessary for this one to work correctly
     'depends': [
         'base',
         'coupon',
+        'documents',
         'sale_coupon',
         'web_editor',
         'website_sale',
