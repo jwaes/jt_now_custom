13ba2212ed3be475c32706d4b89b80ce064e66fc
jwaes <jaco.waes@gmail.com>
2024-06-13 12:55:49 +0000

    post remove theme

---
+++ migrations/17.0.2.0/post-remove_themes.py
@@ -0,0 +1,19 @@
+import logging
+from odoo.upgrade import util
+
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    
+    themes_to_uninstall = [
+        'theme_now',
+    ]
+
+    for candidate in themes_to_uninstall:
+        _logger.info("About to uninstall theme %s", candidate)
+        util.uninstall_theme(cr, candidate)        
+
+    
+
