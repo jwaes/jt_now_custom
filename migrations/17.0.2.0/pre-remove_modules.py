7c5949911793b9ee96caaaf9ee5ecc9f47acd214
jwaes <jaco.waes@gmail.com>
2024-06-13 12:22:02 +0000

    d

--- migrations/17.0.2.0/pre-remove_modules.py
+++ migrations/17.0.2.0/pre-remove_modules.py
@@ -8,10 +8,13 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     
     modules_to_uninstall = [
-        'theme_now',
         'mollie_account_sync', 
-        'payment_mollie_official', 
+        # 'payment_mollie_official', 
 
+    ]
+
+    themes_to_uninstall = [
+        'theme_now',
     ]
 
     modules_to_remove = [
@@ -23,6 +26,10 @@ def migrate(cr, version):
     for candidate in modules_to_uninstall:
         _logger.info("About to uninstall module %s", candidate)
         util.uninstall_module(cr,candidate)
+
+    # for candidate in themes_to_uninstall:
+    #     _logger.info("About to uninstall theme %s", candidate)
+    #     util.uninstall_theme(cr,candidate)        
 
     for candidate in modules_to_remove:
         _logger.info("About to remove module %s", candidate)
