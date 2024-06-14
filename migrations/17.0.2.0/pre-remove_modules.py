5205a9d588564f845bd5a58c986ec60f53327964
jwaes <jaco.waes@gmail.com>
2024-06-14 14:57:55 +0000

    fd

--- migrations/17.0.2.0/pre-remove_modules.py
+++ migrations/17.0.2.0/pre-remove_modules.py
@@ -16,10 +16,6 @@ def migrate(cr, version):
         _logger.info("About to uninstall module %s", candidate)
         util.uninstall_module(cr,candidate)
 
-    # for candidate in themes_to_uninstall:
-    #     _logger.info("About to uninstall theme %s", candidate)
-    #     util.uninstall_theme(cr,candidate)
-
     util.remove_view(cr, xml_id='jt_now_custom.product2')
     util.remove_view(cr, xml_id='jt_now_custom.product_quantity')
     util.remove_view(cr, xml_id='jt_now_custom.shop_product_carousel_square')
