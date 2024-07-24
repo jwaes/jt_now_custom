09574c993ca603b1ee057c0fe0c824d0d2ae9d8b
jwaes <jaco.waes@gmail.com>
2024-07-24 11:45:15 +0000

    fg

--- controllers/variant.py
+++ controllers/variant.py
@@ -4,19 +4,24 @@ from odoo.addons.website_sale.controllers.variant import WebsiteSaleVariantContr
 from odoo.tools.misc import get_lang
 
 class WebsiteSaleStockPropertiesVariantController(WebsiteSaleVariantController):
-    @http.route()
-    def get_combination_info_website(self, product_template_id, product_id, combination, add_qty, **kw):
-        kw['context'] = kw.get('context', {})
-        combination = super().get_combination_info_website(product_template_id, product_id, combination, add_qty, **kw)
+    # @http.route()
+    # def get_combination_info_website(self, product_template_id, product_id, combination, add_qty, **kw):
+    #     kw['context'] = kw.get('context', {})
+    #     combination = super().get_combination_info_website(product_template_id, product_id, combination, add_qty, **kw)
         
-        product = request.env['product.product'].sudo().browse(combination['product_id'])
+    #     product = request.env['product.product'].sudo().browse(combination['product_id'])
 
-        lang = get_lang(request.env).code
+    #     lang = get_lang(request.env).code
 
-        stockinfo_view = request.env['ir.ui.view'].with_context(lang=lang)._render_template('jt_now_custom.website_sale_product_stockinfo', values={
-            'product_variant': product,
-        })
+    #     stockinfo_view = request.env['ir.ui.view'].with_context(lang=lang)._render_template('jt_now_custom.website_sale_product_stockinfo', values={
+    #         'product_variant': product,
+    #     })
 
-        combination['product_stockinfo'] = stockinfo_view
+    #     combination['product_stockinfo'] = stockinfo_view
 
-        return combination
+    #     return combination
+
+    @http.route()
+    def get_combination_info_website(self, *args, **kwargs):
+        request.update_context(website_sale_stock_now=True)
+        return super().get_combination_info_website(*args, **kwargs)        
--- models/product_template.py
+++ models/product_template.py
@@ -59,31 +59,47 @@ class ProductTemplate(models.Model):
                         tmpl.is_dealer_product = True
             
 
-    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, parent_combination=False, only_template=False):
-        combination_info = super(ProductTemplate, self)._get_combination_info(
-            combination=combination,
-            product_id=product_id,
-            add_qty=add_qty,
-            parent_combination=parent_combination,
-            only_template=only_template,
-        )
-        self.ensure_one()
+    # def _get_combination_info(self, combination=False, product_id=False, add_qty=1, parent_combination=False, only_template=False):
+    #     combination_info = super(ProductTemplate, self)._get_combination_info(
+    #         combination=combination,
+    #         product_id=product_id,
+    #         add_qty=add_qty,
+    #         parent_combination=parent_combination,
+    #         only_template=only_template,
+    #     )
+    #     self.ensure_one()
 
-        website = self.env['website'].get_current_website().with_context(self.env.context)
+    #     website = self.env['website'].get_current_website().with_context(self.env.context)
 
-        if website:
-            context = dict(self.env.context, ** {
-                'quantity': self.env.context.get('quantity', add_qty),
-                'pricelist': website.pricelist_id.id
-            })
+    #     if website:
+    #         context = dict(self.env.context, ** {
+    #             'quantity': self.env.context.get('quantity', add_qty),
+    #             'pricelist': website.pricelist_id.id
+    #         })
 
-            product = (self.env['product.product'].browse(combination_info['product_id']) or self)   
+    #         product = (self.env['product.product'].browse(combination_info['product_id']) or self)   
 
-            combination_info.update({
-                'is_dealer_product': product.is_dealer_product,
-            })
+    #         combination_info.update({
+    #             'is_dealer_product': product.is_dealer_product,
+    #         })
 
-        return combination_info
+    #     return combination_info
+
+    def _get_additionnal_combination_info(self, product_or_template, quantity, date, website):
+        res = super()._get_additionnal_combination_info(product_or_template, quantity, date, website)
+
+        if not self.env.context.get('website_sale_stock_now'):
+            return res
+
+        if product_or_template.is_product_variant:
+            product_sudo = product_or_template.sudo()
+            res['is_dealer_product'] = product_sudo.is_dealer_product
+            res['is_stock_product'] = product_sudo.is_stock_product
+            res['lead_time_in_stock'] = product_sudo.lead_time_in_stock
+            res['lead_time_out_stock'] = product_sudo.lead_time_out_stock
+
+        return res
+
 
     def _is_add_to_cart_possible(self, parent_combination=None):
         self.ensure_one()        
