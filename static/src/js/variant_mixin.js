19eaeb4364f3c2e6e2f84368563fc86db003c4ef
jwaes <jaco.waes@gmail.com>
2024-07-25 12:24:06 +0000

    df

--- static/src/js/variant_mixin.js
+++ static/src/js/variant_mixin.js
@@ -3,26 +3,26 @@
 import VariantMixin from "@website_sale/js/variant_mixin";
 import publicWidget from "@web/legacy/js/public/public_widget";
 
-VariantMixin._onChangeCombinationStockInfo = function (ev, $parent, combination) {
+// VariantMixin._onChangeCombinationStockInfo = function (ev, $parent, combination) {
 
-    let product_id = 0;
-    // needed for list view of variants
-    if ($parent.find('input.product_id:checked').length) {
-        product_id = $parent.find('input.product_id:checked').val();
-    } else {
-        product_id = $parent.find('.product_id').val();
-    }
-    const isMainProduct = combination.product_id &&
-        ($parent.is('.js_main_product') || $parent.is('.main_product')) &&
-        combination.product_id === parseInt(product_id);
+//     let product_id = 0;
+//     // needed for list view of variants
+//     if ($parent.find('input.product_id:checked').length) {
+//         product_id = $parent.find('input.product_id:checked').val();
+//     } else {
+//         product_id = $parent.find('.product_id').val();
+//     }
+//     const isMainProduct = combination.product_id &&
+//         ($parent.is('.js_main_product') || $parent.is('.main_product')) &&
+//         combination.product_id === parseInt(product_id);
 
-    if (!this.isWebsite || !isMainProduct) {
-        return;
-    }
+//     if (!this.isWebsite || !isMainProduct) {
+//         return;
+//     }
 
-    $('div.product_stockinfo').html(combination.product_stockinfo).show(); 
+//     $('div.product_stockinfo').html(combination.product_stockinfo).show(); 
 
-};
+// };
 
 VariantMixin._onChangeCombinationStockInfoFixRemove = function (ev, $parent, combination) {
     $('.oe_website_sale')
@@ -38,7 +38,7 @@ publicWidget.registry.WebsiteSale.include({
     _onChangeCombination: function () {
         VariantMixin._onChangeCombinationStockInfoFixRemove.apply(this, arguments);
         this._super.apply(this, arguments);
-        VariantMixin._onChangeCombinationStockInfo.apply(this, arguments);
+        // VariantMixin._onChangeCombinationStockInfo.apply(this, arguments);
     },
 });
 
