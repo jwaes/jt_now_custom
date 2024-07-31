331f5d389fe05accf2d65db68a8f0f064f012fb5
jwaes <jaco.waes@gmail.com>
2024-07-31 12:19:19 +0000

    df

---
+++ models/product_pricelist.py
@@ -0,0 +1,16 @@
+import logging
+import re
+import json
+from odoo import api, fields, models
+from datetime import datetime, timedelta
+
+_logger = logging.getLogger(__name__)
+
+class Pricelist(models.Model):
+    _inherit = "product.pricelist"
+
+    def _get_applicable_rules_domain(self, products, date, **kwargs):
+        res = super()._get_applicable_rules_domain(products, date, **kwargs)
+        _logger.info("_get_applicable_rules_domain")
+        _logger.info(json.dumps(res))
+        return res
