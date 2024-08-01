b74b28a9bf5fe38d95e4bc2476920fb6bdf7bb98
jwaes <jaco.waes@gmail.com>
2024-08-01 06:40:13 +0000

    df

--- models/product_pricelist.py
+++ models/product_pricelist.py
@@ -18,14 +18,14 @@ class Pricelist(models.Model):
 
     def _get_products_price(self, products, *args, **kwargs):
         res = super()._get_products_price(products, *args, **kwargs)
-        
+        _logger.info("_get_products_price")
+        _logger.info(json.dumps(res, sort_keys=True, default=str))
         variants = products.filtered(lambda r: r._name == 'product.template').product_variant_id
         if variants:
             variant_res = super()._get_products_price(variants, *args, **kwargs)
-            for record in self:
-                for tmpl in res[record.id]:
-                    res[record.id] = variant_res[record.product_variant_id.id]
-        
+            for record in res:
+                res[record.id] = variant_res[record.product_variant_id.id]
+        _logger.info("aftger")
         _logger.info(json.dumps(res, sort_keys=True, default=str))
 
         return res
